fa6d7c4c9171229043d5460c5e450c5cfcb8e0bf
mham <warofzen1@proton.me>
2024-04-13 20:35:13 +0700

    changes

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -gx EDITOR neovim
+# set -gx QT_QPA_PLATFORMTHEME qt6ct
+set -gx QT_WAYLAND_DECORATION adwaita
